32eddae3f97e9376c9ce37b9d5ba4103b7a3139a
SharafatKarim <sharafat2004@gmail.com>
2023-05-30 13:03:13 +0600

    Q

--- codeforces/Assiut University Training - Newcomers/Sheet #2 (Loops)/Q. Digits.cpp
+++ codeforces/Assiut University Training - Newcomers/Sheet #2 (Loops)/Q. Digits.cpp
@@ -3,20 +3,28 @@ using namespace std;
 
 int main()
 {
+    int test_case;
+    cin >> test_case;
+
+    long long array[test_case];
+    for (int i = 0; i < test_case; i++)
+        cin >> array[i];
+
     int n;
-    cin >> n;
-    // print n digits
-    int temp = n;
-    int digits = 0;
-    while (temp != 0)
+    for (int i = 0; i < test_case; i++)
     {
-        temp /= 10;
-        digits++;
-    }
-    for (int i = 0; i < digits; i++)
-    {
-        int digit = n % 10;
-        n /= 10;
-        cout << digit << endl;
+        n = array[i];
+        if (n != 0)
+        {
+            // print digits of n
+            while (n)
+            {
+                cout << n % 10 << " ";
+                n /= 10;
+            }
+            cout << endl;
+        }
+        else
+            cout << 0 << endl;
     }
 }
