42af99d66d28228f2ea9aec8f72c0bf475086af0
SharafatKarim <sharafat2004@gmail.com>
2025-01-04 21:26:07 +0600

    Auto push at 21:26:00

---
+++ codeforces/problemsets/A_MEX_Table.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Simple template by sharafat 
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<string, string> pss;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
+#define endl '\n'
+
+void test() {
+    ll n, m;
+    cin >> n >> m;
+
+    cout << (m+1) << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // Code starts here
+
+    ll t = 1;
+    cin >> t;
+    while(t--) test();
+
+    // Code ends here
+    return 0;
+}
