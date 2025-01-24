124e04a6376c744afce1fdeb4185e1fcc64fd7b4
SharafatKarim <sharafat2004@gmail.com>
2025-01-24 11:14:43 +0600

    Auto push at 11:14:40

---
+++ codeforces/problemsets/C_Increasing_Sequence_with_Fixed_OR.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Simple template by sharafat 
+typedef long long ll;
+typedef vector<ll> vl;
+
+#define all(s) s.begin(), s.end()
+#define rall(s) s.rbegin(), s.rend()
+ 
+#define endl '\n'
+
+void Test(ll t) {
+    ll n;
+    cin >> n;
+
+    vl bin;
+    for (ll i=0; i<60; i++) {
+      bin.push_back( n |  );
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // Code starts here
+
+    ll t = 1;
+    cin >> t;
+    for (ll i=0; i<t; i++) Test(t); 
+
+    // Code ends here
+    return 0;
+}
