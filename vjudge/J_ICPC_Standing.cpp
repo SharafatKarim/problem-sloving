5e8d472cc7bd2ea1aedd3fdf9eec93bdc4b1399f
SharafatKarim <sharafat2004@gmail.com>
2025-10-24 18:06:00 +0600

    Auto push at 18:05:54

---
+++ vjudge/J_ICPC_Standing.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Simple template by sharafat 
+typedef long long ll;
+typedef vector<ll> vl;
+
+#define all(s) s.begin(), s.end()
+#define rall(s) s.rbegin(), s.rend()
+ 
+#define endl '\n'
+
+void Test(ll t) {
+    cout << "Case " << t << ": Yes" << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // Code starts here
+
+    ll t = 1;
+    cin >> t;
+    for (ll i=0; i<t; i++) Test(i+1); 
+
+    // Code ends here
+    return 0;
+}
