98c8b70b061acb2ce7d6c8825d4dbf50fb4efd49
SharafatKarim <sharafat2004@gmail.com>
2025-12-11 18:46:53 +0600

    Auto push at 18:46:50

---
+++ vjudge/B_String.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Simple template by sharafat 
+typedef long long ll;
+typedef vector<ll> vl;
+
+#define all(s) s.begin(), s.end()
+#define rall(s) s.rbegin(), s.rend()
+ 
+#define endl '\n'
+
+void Test(ll t) {
+    string s; ll ct=0;
+    cin >> s;
+    for (auto i:s) {
+        if (i == '1') ct++;
+    }
+    cout << ct << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // Code starts here
+
+    ll t = 1;
+    cin >> t;
+    for (ll i=0; i<t; i++) Test(t); 
+
+    // Code ends here
+    return 0;
+}
